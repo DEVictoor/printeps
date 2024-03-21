0d51f98676ac019e04bbba1f7e351039acb8d8ef
Victor Fernando Mireles Bernabé <devictormireles@gmail.com>
2024-03-21 13:20:23 -0500

    add: cors in api/print

--- application/controllers/tickets.php
+++ application/controllers/tickets.php
@@ -1,8 +1,18 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Content-Type: application/json");
+
 class Tickets extends CI_Controller
 {
+	public function __construct()
+	{
+		header('Access-Control-Allow-Origin: *');
+		parent::__construct();
+	}
+
 	public function print()
 	{
 		$data["atencion"][0]["ticket"] = $_GET["ticket"];
